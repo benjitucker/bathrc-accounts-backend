cd7c5f53e6eab3af0e9c8908cdee2fc63d3dc3c0
Benjamin Tucker <benjamin.tucker@kontron.com>
2025-12-29 10:59:58 +0000

    Handle training requests enhancement

--- handle_training.go
+++ handle_training.go
@@ -2,31 +2,92 @@ package main
 
 import (
 	"benjitucker/bathrc-accounts/db"
+	"benjitucker/bathrc-accounts/email"
 	"benjitucker/bathrc-accounts/jotform-webhook"
-
-	"github.com/go-kit/log/level"
+	"errors"
+	"fmt"
+	"math"
+	"strconv"
+	"strings"
+	"time"
 )
 
 func handleTrainingRequest(formData *jotform_webhook.FormData, request jotform_webhook.TrainingRawRequest) error {
 
-	submission := db.TrainingSubmission{
-		Date:             request.SelectSession.Date,
-		DateUnix:         request.SelectSession.Date.Unix(),
-		MembershipNumber: request.MembershipNumber,
+	amount, err := strconv.ParseFloat(request.Amount, 64)
+	if err != nil {
+		return fmt.Errorf("amount float number (%s): %w", request.Amount, err)
 	}
+	amountPence := math.Floor(amount * 100)
+	requestDate := time.Time(request.SubmitDate)
+	currentMembership := len(request.CurrentMembershipSelection[0]) > 0
 
-	err := trainTable.Put(&submission, formData.SubmissionID)
+	submission := db.TrainingSubmission{
+		Date:              request.SelectSession.Date,
+		DateUnix:          request.SelectSession.Date.Unix(),
+		MembershipNumber:  strings.Trim(request.MembershipNumber, " "),
+		RequestCurrMem:    currentMembership,
+		Venue:             request.SelectedVenue,
+		AmountPence:       int64(amountPence),
+		HorseName:         request.HorseName,
+		RequestDate:       requestDate,
+		RequestDateUnix:   requestDate.Unix(),
+		PaymentReference:  request.PaymentReference,
+		FoundMemberRecord: true,
+		AlreadyBooked:     false,
+		MembershipCorrect: true,
+	}
+	err = trainTable.Put(&submission, formData.SubmissionID)
 	if err != nil {
 		return err
 	}
 
-	// TODO:
 	// Check membership number
-	// email me on invalid membership number incase its a new member
-	// check that the membership is current, and flag inconsistency with the form data with member
-	// check that a training request for the same date/time has not already been received
-	// email member to confirm that their training request has been received, pending payment
+	memberRecord, err := memberTable.Get(submission.MembershipNumber)
+	if err != nil {
+		// email me on invalid membership number incase it's a new member
+		err2 := fmt.Errorf("membership record for number (%s): %w", submission.MembershipNumber, err)
 
+		submission.FoundMemberRecord = false
+		// update
+		err = trainTable.Put(&submission, formData.SubmissionID)
+		if err != nil {
+			err2 = errors.Join(err2, err)
+		}
+		return err2
+	}
+
+	// check that the membership is current, and flag inconsistency with the form data with member
+	membershipOnTrainingDate := membershipDateCheck(
+		memberRecord.MembershipValidFrom, memberRecord.MembershipValidTo, &submission.Date)
+
+	if membershipOnTrainingDate != submission.RequestCurrMem {
+		// email me on invalid membership incase it's a new member
+		err2 := fmt.Errorf("membership check for %s %s failed", memberRecord.FirstName, memberRecord.LastName)
+
+		submission.MembershipCorrect = false
+		// update
+		err = trainTable.Put(&submission, formData.SubmissionID)
+		if err != nil {
+			err2 = errors.Join(err2, err)
+		}
+		return err2
+	}
+
+	// TODO
+	// Email membership inconsistency
+	//emailHandler.SendEmail(memberRecord.Email,
+
+	// TODO:
+	// check that a training request for the same date/time has not already been received
+
+	// email member to confirm that their training request has been received, pending payment
+	emailHandler.SendConfirm(memberRecord.Email, &email.ConfirmData{
+		Name: memberRecord.FirstName,
+		Link: memberRecord.ClubMembershipStatus,
+	})
+
+	/* TODO remove:
 	records, err := trainTable.GetAll()
 	if err != nil {
 		return err
@@ -36,6 +97,21 @@ func handleTrainingRequest(formData *jotform_webhook.FormData, request jotform_w
 	for _, record := range records {
 		_ = level.Debug(logger).Log("msg", "Handle Request", "record from db", record)
 	}
+	*/
 
 	return nil
+}
+
+func membershipDateCheck(start, end, target *time.Time) bool {
+	if start == nil {
+		return false
+	}
+	if end == nil {
+		return false
+	}
+	if target == nil {
+		return false
+	}
+	return (target.Equal(*start) || target.After(*start)) &&
+		(target.Equal(*end) || target.Before(*end))
 }
